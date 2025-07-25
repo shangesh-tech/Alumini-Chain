4131e1702e1f0da7730677f3a0509a9571adc6bc
Shangesh S <shangesh2006@gmail.com>
2025-07-25 11:16:08 +0530

    Update layout.js

--- client/app/layout.js
+++ client/app/layout.js
@@ -30,7 +30,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-zinc-900`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased `}
       >
         <div className="flex h-screen">
           <Sidebar />
