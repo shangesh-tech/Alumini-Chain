347939c3d38919f2aaabbb0b592ef82faea1f3ab
Shangesh S <shangesh2006@gmail.com>
2025-08-09 21:56:40 +0530

    feat: Add auth.js config

--- client/app/layout.js
+++ client/app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { SessionProvider } from "next-auth/react";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
